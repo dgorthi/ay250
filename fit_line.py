f0c488fd4acca3cbeb8a43b0c12f42006f054714
Deepthi <deepthigorthi136@gmail.com>
2016-09-09 15:05:57 -0700

    modified sampler, fitting fake data-try 1

--- fit_line.py
+++ fit_line.py
@@ -1,19 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import emcee
+import corner
 
 def fake_data(m,b,N):
     """Generates N fake data points for the straight line of slope m and
-    intercept b, with gaussian uncertainities for each data point
+    intercept b, with gaussian uncertainities for each data point.
 
     """
     x = np.random.uniform(-10,10,N)
-    err = np.random.normal(loc=0,scale=1,size=N)
+    err = np.random.normal(loc=0,scale=2,size=N)
     y = m*x + b + err
     
-    f,ax = plt.subplots(1,1)
-    ax.errorbar(x,y,yerr=err,fmt='o')
-    plt.show()
+    return [x,y,err]
+
+def lnP(theta,data):
+    """Generates the log likelihood probability of the given parameters
+    (m,b) fitting the data, from the inverse squared deviation of the
+    fit. Assuming flat priors. Formula: P(m,b|D) = P(D|m,b)P(m|b)P(b)
+
+    """
+    err = np.sum((data[1]-(theta[1]*data[0]+theta[0]))**2)
+    return (100/err)
     
-    return y
+def next(theta):
+    """The proposal density is a normal distribution of width 5
+    (determined by hit and trial for this particular problem),
+    centered around theta.
+
+    """
+    return np.random.normal(loc=theta,scale=1)
+
+def markov_chain(dat):
+    """Generates a markov chain to sample a given log likelihood
+    function. Calls functions lnP() and next() to compute the log
+    likelihood of a given parameter set and the generate the next
+    parameter set, respectively.
+
+    """
+    theta0 = [0,0]              #Starting point
+    theta1 = next(theta0)
+    Pcurr  = lnP(theta0,dat)
+
+    theta,a= theta0,np.array([])
+    
+    for i in range(10**4):
+        theta1 = next(theta0)
+        Pnext  = lnP(theta1,dat)       
+        a = np.append(a,min(1,np.exp(Pnext-Pcurr)))
+        if a[i]==1:
+            theta = np.vstack((theta,theta1))
+            theta0 = theta1; Pcurr = Pnext
+            continue
+        elif a[i]>np.random.ranf():
+            theta = np.vstack((theta,theta1))
+        else:
+            theta = np.vstack((theta,theta0))
+    print('Scale:%d\tMean acceptance ratio:%f\n'%(4,np.mean(a)))
+    return theta,a
+
+data = fake_data(m=5,b=-2,N=100)
+
+f,ax = plt.subplots(1,1)
+ax.errorbar(data[0],data[1],yerr=data[2],fmt='o')
+
+theta,a = markov_chain(data)
+figure = corner.corner(theta)
+
+plt.show()
+#line_fit = np.dot(inv(np.dot(np.dot(A.T,inv(C)),A)),np.dot(np.dot(A.T,inv(C)),Y))
+
 
--- mcmc_sampler.py
+++ mcmc_sampler.py
@@ -2,50 +2,68 @@
 ## from a one dimensional Gaussian distribution.
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.mlab as mlab
 
 def lnP(mu,sigma,x):
-    """The posterior distribution from which samples will be drawn."""
+    """The posterior distribution from which samples will be drawn.
+    
+    """
     return (-0.5*(mu-x)**2/sigma**2)
 
 def next(x):
-    """The proposal density is a normal distribution of width 1, centered
-    around the sample with higher probability between the current and
-    the previous."""
-    return (np.random.normal(loc=x,scale=5))
+    """The proposal density is a normal distribution of width 5
+    (determined by hit and trial for this particular problem),
+    centered around the sample with higher probability between the
+    current and the previous.
+
+    """
+    return (np.random.normal(loc=x,scale=3))
 
 def markov_chain():
-    x0  = 0; x1  = next(x0)      #Starting point
+    """Generates a markov chain to sample a given log likelihood function.
+
+    """
+    x0  = np.random.rand(1); x1  = next(x0)      #Starting point
     x,a = np.array([]),np.array([])
     
-    for i in range(10**5):
+    for i in range(10**4):
+        x1 = next(x0)
         a = np.append(a,min(1,np.exp(lnP(5,1,x1) - lnP(5,1,x0))))
         if a[i]==1:
             x = np.append(x,x1)
-            x0 = x1; x1 = next(x1)
+            x0 = x1 
             continue
         elif a[i]>np.random.ranf():
             x = np.append(x,x1)
         else:
             x = np.append(x,x0)
-        tmp = x1
-        x1 = next(x0)
-        x0 = tmp
-    print('Mean acceptance ratio:%f\n'%np.mean(a))
+    print('Scale:%d\tMean acceptance ratio:%f\n'%(4,np.mean(a)))
     return x
 
-f,ax = plt.subplots(1,1)
-ax.hist(markov_chain(),bins=np.linspace(0,10,21))
-ax.set_xlabel('x')
-ax.set_ylabel('Frequency')
-#ax.plot(x)
+x = markov_chain()
+y = lnP(5,1,x)
+
+f1,(ax1,ax2) = plt.subplots(2,1,sharex=True)
+hist=ax1.hist(x,bins=100)
+bins=(hist[1][1:]+hist[1][:-1])/2
+ax1.plot(bins,mlab.normpdf(bins,5,1))
+ax1.set_ylabel('Frequency')
+
+ax2.plot(x,y,'.')
+ax2.set_xlabel('x')
+ax2.set_ylabel('LnP(x)')
+
+f2,ax3 = plt.subplots(1,1)
+ax3.plot(y)
+ax3.set_xlabel('Step Number')
+ax3.set_ylabel('Ln(P)')
 
 plt.show()
 
 ## next_perform was written to check the performance of
 ## next(). Specifically, to check if the width of the gaussian
 ## affected the nature of samples drawn. Conclusion: Too large a width
-## or too small a width both cause the next function to diverge, 1 was
+## or too small a width both cause the next function to diverge, 5 was
 ## found to be optimal by hit and trial.
 # def next_perform():
 #     k = np.array([])
