5e2b4ba181f87444dbe9abf82098853ec75a099e
DeepthiGorthi23 <deepthigorthi136@gmail.com>
2016-09-09 22:31:38 -0700

    modified fit_line to give a resonable acceptance rate

--- fit_line.py
+++ fit_line.py
@@ -1,7 +1,14 @@
+## Program to generate fake data with m=5, b=-2 and then to fit this
+## fake data using a MCMC sampler implemented using the
+## Metropolis-Hastings algorithm.
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import emcee
 import corner
+
+sns.set_style("whitegrid")
+sns.set_context("notebook",font_scale=2)
 
 def fake_data(m,b,N):
     """Generates N fake data points for the straight line of slope m and
@@ -9,19 +16,19 @@ def fake_data(m,b,N):
 
     """
     x = np.random.uniform(-10,10,N)
-    err = np.random.normal(loc=0,scale=2,size=N)
+    err = np.random.normal(loc=0,scale=3,size=N)
     y = m*x + b + err
     
-    return [x,y,err]
+    return x,y,err
 
-def lnP(theta,data):
+def lnP(theta,x,y):
     """Generates the log likelihood probability of the given parameters
     (m,b) fitting the data, from the inverse squared deviation of the
     fit. Assuming flat priors. Formula: P(m,b|D) = P(D|m,b)P(m|b)P(b)
 
     """
-    err = np.sum((data[1]-(theta[1]*data[0]+theta[0]))**2)
-    return (100/err)
+    err = np.sum((y-(theta[1]*x+theta[0]))**2)
+    return (1/err)
     
 def next(theta):
     """The proposal density is a normal distribution of width 5
@@ -29,9 +36,9 @@ def next(theta):
     centered around theta.
 
     """
-    return np.random.normal(loc=theta,scale=1)
+    return np.random.normal(loc=theta,scale=[2,1])
 
-def markov_chain(dat):
+def markov_chain(x,y):
     """Generates a markov chain to sample a given log likelihood
     function. Calls functions lnP() and next() to compute the log
     likelihood of a given parameter set and the generate the next
@@ -40,14 +47,14 @@ def markov_chain(dat):
     """
     theta0 = [0,0]              #Starting point
     theta1 = next(theta0)
-    Pcurr  = lnP(theta0,dat)
+    Pcurr  = lnP(theta0,x,y)
 
     theta,a= theta0,np.array([])
     
     for i in range(10**4):
         theta1 = next(theta0)
-        Pnext  = lnP(theta1,dat)       
-        a = np.append(a,min(1,np.exp(Pnext-Pcurr)))
+        Pnext  = lnP(theta1,x,y)       
+        a = np.append(a,min(1,Pnext/Pcurr))
         if a[i]==1:
             theta = np.vstack((theta,theta1))
             theta0 = theta1; Pcurr = Pnext
@@ -56,16 +63,19 @@ def markov_chain(dat):
             theta = np.vstack((theta,theta1))
         else:
             theta = np.vstack((theta,theta0))
-    print('Scale:%d\tMean acceptance ratio:%f\n'%(4,np.mean(a)))
-    return theta,a
+    print('Mean acceptance ratio:%f\n'%np.mean(a))
+    return theta[1:],a
 
-data = fake_data(m=5,b=-2,N=100)
+x,y,err = fake_data(m=5,b=-2,N=100)
+theta,a = markov_chain(x,y)
 
-f,ax = plt.subplots(1,1)
-ax.errorbar(data[0],data[1],yerr=data[2],fmt='o')
+f1,ax = plt.subplots(1,1)
+ax.errorbar(x,y,yerr=err,fmt='o')
+#ax.plot((theta[0]+theta[1]*x))
+ax.set_xlabel('x')
+ax.set_ylabel('y')
 
-theta,a = markov_chain(data)
-figure = corner.corner(theta)
+f2 = corner.corner(theta,bins=50,labels=('intercept','slope'),plot_contours=True,top_ticks=True,truths=[-2.,5.])
 
 plt.show()
 #line_fit = np.dot(inv(np.dot(np.dot(A.T,inv(C)),A)),np.dot(np.dot(A.T,inv(C)),Y))
--- mcmc_sampler.py
+++ mcmc_sampler.py
@@ -1,8 +1,12 @@
-## My own Metropolis-Hastings algorithm, which is tested by sampling
-## from a one dimensional Gaussian distribution.
+## A Metropolis-Hastings algorithm, which is tested by sampling from a
+## one dimensional Gaussian distribution.
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import seaborn as sns
+
+sns.set_style("whitegrid")
+sns.set_context("notebook",font_scale=2)
 
 def lnP(mu,sigma,x):
     """The posterior distribution from which samples will be drawn.
@@ -11,7 +15,7 @@ def lnP(mu,sigma,x):
     return (-0.5*(mu-x)**2/sigma**2)
 
 def next(x):
-    """The proposal density is a normal distribution of width 5
+    """The proposal density is a normal distribution of width 3
     (determined by hit and trial for this particular problem),
     centered around the sample with higher probability between the
     current and the previous.
@@ -23,8 +27,8 @@ def markov_chain():
     """Generates a markov chain to sample a given log likelihood function.
 
     """
-    x0  = np.random.rand(1); x1  = next(x0)      #Starting point
-    x,a = np.array([]),np.array([])
+    x0  = 0; x1  = next(x0)                      #Starting point
+    x,a = np.array([]),np.array([])              #markov-chain and acceptance rate
     
     for i in range(10**4):
         x1 = next(x0)
@@ -37,26 +41,30 @@ def markov_chain():
             x = np.append(x,x1)
         else:
             x = np.append(x,x0)
-    print('Scale:%d\tMean acceptance ratio:%f\n'%(4,np.mean(a)))
+    print('Mean acceptance ratio:%f\n'%np.mean(a))
     return x
 
 x = markov_chain()
 y = lnP(5,1,x)
 
-f1,(ax1,ax2) = plt.subplots(2,1,sharex=True)
-hist=ax1.hist(x,bins=100)
-bins=(hist[1][1:]+hist[1][:-1])/2
-ax1.plot(bins,mlab.normpdf(bins,5,1))
-ax1.set_ylabel('Frequency')
+#Diagnostic plots
+f1,(ax1,ax2) = plt.subplots(1,2)
+ax1.plot(y)
+ax1.set_xlabel('Step Number')
+ax1.set_ylabel('LnP(x)')
 
-ax2.plot(x,y,'.')
+ax2.hist(x,bins=100)
+ax2.set_ylabel('Frequency')
 ax2.set_xlabel('x')
-ax2.set_ylabel('LnP(x)')
 
+x_true= np.linspace(0,10,1000)
+y_true= np.log(mlab.normpdf(x_true,5,1))
 f2,ax3 = plt.subplots(1,1)
-ax3.plot(y)
-ax3.set_xlabel('Step Number')
-ax3.set_ylabel('Ln(P)')
+ax3.plot(x,y,'.',label='Log likelihood')
+ax3.plot(x_true,y_true,label='True Distribution')
+ax3.set_xlabel('x')
+ax3.set_ylabel('LnP(x)')
+ax3.legend()
 
 plt.show()
 
